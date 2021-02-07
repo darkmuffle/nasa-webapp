0d55cc81889534e046b9228db36d3cba01fe2d49
darkmuffle <redaib@DESKTOP-EHHQM65.localdomain>
2021-02-07 22:00:50 +0100

    adding express server

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
-app.use(express.static('./dist/<name-on-package.json>'));
-app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/<name-on-package.json>/'}
-  );
-  });
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/nasa-webtest'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/nasa-webtest/'}),
+);
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
